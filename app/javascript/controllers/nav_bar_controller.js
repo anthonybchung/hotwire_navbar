ebfe17facc95755d98bafee4697d2c5c2002ba7c
abchung <anthony.b.chung@gmail.com>
2023-06-04 09:50:33 +1000

    menu icon changes relative to sliding menu

--- app/javascript/controllers/nav_bar_controller.js
+++ app/javascript/controllers/nav_bar_controller.js
@@ -3,10 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="nav-bar"
 export default class extends Controller {
 
-  static targets = ["menu"]
+  static targets = ["menu","hamburger","cross"]
   static values = {opened: Boolean}
   initialize() {
     this.openedValue = false
+    this.hamburgerTarget.classList.remove("hidden")
+    this.crossTarget.classList.add("hidden")
     document.body.style.overflowY = "auto"
   }
 
@@ -17,11 +19,20 @@ export default class extends Controller {
     if(this.openedValue){
       this.menuTarget.classList.remove("h-screen")
       this.menuTarget.classList.add("h-0")
+
+      this.hamburgerTarget.classList.remove("hidden")
+      this.crossTarget.classList.add("hidden")
+      console.log("hamburger")
+      console.log(this.hamburgerTarget)
+
       document.body.style.overflowY = "auto"
       this.openedValue = !this.openedValue
     } else {
       this.menuTarget.classList.remove("h-0")
       this.menuTarget.classList.add("h-screen")
+
+      this.crossTarget.classList.remove("hidden")
+      this.hamburgerTarget.classList.add("hidden")
       document.body.style.overflowY = "hidden"
       this.openedValue = !this.openedValue
     }
